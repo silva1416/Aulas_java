10c003a37cb03e999bb2374e4a03fcd1aab5b1e0
aluno <aluno@DSN1251017059>
2022-04-11 15:32:12 -0300

    .

---
+++ OrientacaoaObjeto/src/main.java
@@ -0,0 +1,16 @@
+
+public class main {
+
+	public static void main(String[] args) {
+ pessoa pessoa1= new pessoa ();
+		 
+		 pessoa1.nome = "Sthe";
+		 pessoa1.numFigurinhas = 1;
+		 
+		 pessoa1.receber(3);
+		 
+		 System.out.println(pessoa1.nome);
+		 System.out.println(pessoa1.numFigurinhas);
+	}
+
+}
---
+++ OrientacaoaObjeto/src/pessoa.java
@@ -0,0 +1,12 @@
+
+public class pessoa {
+	String nome;
+	int numFigurinhas;
+	
+	void receber(int numFigurinhas) {
+		this.numFigurinhas += numFigurinhas;
+		
+	}
+   void dar() {
+   }
+}
