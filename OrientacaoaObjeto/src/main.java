324e7bebd2a59234246b80082fae44d9e5a9cf46
aluno <aluno@DSN1251017050>
2022-04-13 11:13:50 -0300

    Troca de figurinha

--- OrientacaoaObjeto/src/main.java
+++ OrientacaoaObjeto/src/main.java
@@ -2,15 +2,37 @@
 public class main {
 
 	public static void main(String[] args) {
+//Criação da 1 pessoa
  pessoa pessoa1= new pessoa ();
 		 
 		 pessoa1.nome = "Sthe";
-		 pessoa1.numFigurinhas = 1;
-		 
-		 pessoa1.receber(3);
+		 pessoa1.numFigurinhas = 4;
+
+//Metodo receber
+		 pessoa1.receber(0);
 		 
 		 System.out.println(pessoa1.nome);
 		 System.out.println(pessoa1.numFigurinhas);
-	}
 
+//Criação da 2 pessoa
+ pessoa pessoa2 = new pessoa();
+ 
+        pessoa2.nome = "Gislene";
+        pessoa2.numFigurinhas = 5;
+        
+        System.out.println(pessoa2.nome);
+        System.out.println(pessoa2.numFigurinhas);
+        
+  //metodo dar      
+        boolean msg = pessoa1.dar(1000,pessoa2);
+        if(msg==true) {
+ //mostrar quanto tinha antes e depois de dar
+        System.out.println(pessoa1.nome);
+        System.out.println(pessoa1.numFigurinhas);
+        
+        System.out.println(pessoa2.nome);
+        System.out.println(pessoa2.numFigurinhas);
+    
+	}
+	}
 }
--- OrientacaoaObjeto/src/pessoa.java
+++ OrientacaoaObjeto/src/pessoa.java
@@ -1,12 +1,33 @@
 
 public class pessoa {
+//Criando os dados/atributos
 	String nome;
 	int numFigurinhas;
-	
+
+//Metodos
 	void receber(int numFigurinhas) {
 		this.numFigurinhas += numFigurinhas;
 		
 	}
-   void dar() {
+//metodos
+//assinatura do metodo(tipo de retorno, metodo , 
+   boolean dar(int Qtdfig, pessoa pessoa) {
+	   
+	   boolean msg;
+	   
+	   if (this.numFigurinhas < Qtdfig) {
+		   System.out.println("Não foi possivel completar sua ação.");
+		  // msg = false;
+		   return false;
+	
+	   }else {
+	   this.numFigurinhas -= numFigurinhas;
+	   pessoa.receber(numFigurinhas);
+	  // msg = true;
+	   return true;
+	   
+	    }
+        // return msg;
+         //no segundo caso tem retorno aqui embaixo 
    }
 }
