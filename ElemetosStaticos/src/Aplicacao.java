196d2284284d8d7bcfb4ce2ef766003a7504dc6c
silva1416 <102963024+silva1416@users.noreply.github.com>
2022-05-02 11:32:01 -0300

    Add files via upload
    .

---
+++ ElemetosStaticos/src/Aplicacao.java
@@ -0,0 +1,39 @@
+
+public class Aplicacao {
+
+	public static void main(String[] args) {
+		
+		Matematica matematica = new Matematica();
+		
+//		int resultado = matematica.somar(2, 3);
+//        System.out.println(resultado);
+//        resultado = matematica.subtrair(10, 4);
+//        System.out.println(resultado);
+//		
+//	int resultado = Matematica.somar(2, 4);
+//	System.out.println(resultado);	
+//	resultado = Matematica.subtrair(10, 11);
+//	System.out.println(resultado);	
+	
+//	Contador contador1 = new Contador();
+//	Contador contador2 = new Contador();
+//	Contador contador3 = new Contador();
+//	
+//	contador1.incrementar(); //vai aparecer o mesmo valor em todos pois é estatico e é compartilhado por todos 
+//	
+//	System.out.println(contador1.getValor());
+//	System.out.println(contador2.getValor());
+//	System.out.println(contador3.getValor());
+		
+//	Contador.incrementar();
+//	
+//	
+//	System.out.println(Contador.getValor());
+		
+	System.out.println(Contastantes.MEDIA_DA_PROVA);
+	
+	
+	
+	
+	}
+}
---
+++ ElemetosStaticos/src/Contastantes.java
@@ -0,0 +1,12 @@
+
+public class Contastantes {
+	
+//	public static final double MEDIA_DA_PROVA = 7.0; posso preestabelecer assim ou assim:
+	public static final double MEDIA_DA_PROVA;
+	
+	static{
+		MEDIA_DA_PROVA = 7.0;
+	}
+	
+	
+}
