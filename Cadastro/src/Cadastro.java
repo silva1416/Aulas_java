1c19e023eb76649209cdd709fe0056a40de17dc6
silva1416 <102963024+silva1416@users.noreply.github.com>
2022-05-18 11:44:11 -0300

    Add files via upload

---
+++ Cadastro/src/Cadastro.java
@@ -0,0 +1,35 @@
+
+public class Cadastro {
+	String nome;
+	int idade;
+	String bairro;
+	String rua;
+	int n;
+	
+	
+	public Cadastro(String nome, int idade, String bairro, String rua, int n) {
+		super();
+		this.nome = nome;
+		this.idade = idade;
+		this.bairro = bairro;
+		this.rua = rua;
+		this.n = n;
+	}
+
+
+
+	@Override
+	public String toString() {
+		return "Cadastro [nome=" + nome + ", idade=" + idade + ", bairro=" + bairro + ", rua=" + rua + ", n=" + n + "]";
+	}
+	
+	
+	
+	
+	
+	}
+	
+	
+	
+	
+
