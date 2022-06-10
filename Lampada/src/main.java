62a17dad394a823b8be48146f71c12c6b6452d75
silva1416 <102963024+silva1416@users.noreply.github.com>
2022-06-10 10:47:12 -0300

    Update main.java

--- Lampada/src/main.java
+++ Lampada/src/main.java
@@ -3,14 +3,14 @@ public class main {
 
 	public static void main(String[] args) {
 		
-	Lampada lampada1 = new Lampada();
+	Lampada lampada1 = new Lampada();//cria obj
 	
-	lampada1.imprimir();
+	lampada1.imprimir();//manda mostrar
 	
-	lampada1.desligar();
+	lampada1.desligar();//mnada desligar ja que começou ligada
 //	lampada1.imprimir();
 	
-	lampada1.ligar();
+	lampada1.ligar();//manda ligar
 //	lampada1.imprimir();
 	
 	
