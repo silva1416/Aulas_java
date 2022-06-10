0ac95f559e3444ec03a7816031ae4f9d8e0f1b59
silva1416 <102963024+silva1416@users.noreply.github.com>
2022-06-10 10:32:40 -0300

    Update Aplicacao.java

--- PolimorfismoBebidas1/src/Aplicacao.java
+++ PolimorfismoBebidas1/src/Aplicacao.java
@@ -3,13 +3,14 @@ public class Aplicacao {
 
 	public static void main(String[] args) {
 		
-		Preparador preparador = new Preparador();
+		Preparador preparador = new Preparador();//cria preparador
 
-		LeiteQuente leitequente = new LeiteQuente();
-		preparador.prepararBebida(leitequente);
+		LeiteQuente leitequente = new LeiteQuente();//cria leite quente
+		preparador.prepararBebida(leitequente);//manda preparar leite quente
 		
-		Refrigerante refrigerante = new Refrigerante();
-		preparador.prepararBebida(refrigerante);
+		Refrigerante refrigerante = new Refrigerante();//cria refrigerante
+		preparador.prepararBebida(refrigerante);//manda preparar refrigerante
+		
 
 	}
 
