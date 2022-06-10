1ac96b76ad80bdc674146f632a75328f895b2a07
silva1416 <102963024+silva1416@users.noreply.github.com>
2022-06-10 10:51:40 -0300

    Update Aplicacao.java

--- CalculoArea/src/Aplicacao.java
+++ CalculoArea/src/Aplicacao.java
@@ -4,21 +4,21 @@ public class Aplicacao {
 
 	public static void main(String[] args) {
 		Scanner scanner =  new Scanner(System.in);
-		
+		//cria area
 		area area = new area();
 		
 		System.out.println("Calculo Area");
 		System.out.println("Digite a base:");
-		int b = scanner. nextInt();
+		int b = scanner. nextInt();//guarda na var
 		
 		System.out.println("Digite a altura");
-		int h = scanner. nextInt();
+		int h = scanner. nextInt();//guarda na var.
 		
-		double Areaf = area.area(b, h);
-		System.out.println("A area da figura é de: " + Areaf);
+		double Areaf = area.area(b, h);//calcula
+		System.out.println("A area da figura Ã© de: " + Areaf);//mostra
 		
 		double Areaq = area.areaq(b);
-		System.out.println("A area do quadrado é: " + Areaq);
+		System.out.println("A area do quadrado Ã©: " + Areaq);
 		
 		}
 
