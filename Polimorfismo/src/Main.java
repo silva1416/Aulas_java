fd8113635487adeb6c7d933fb7507a0ff00924fb
silva1416 <102963024+silva1416@users.noreply.github.com>
2022-06-10 10:35:09 -0300

    Update Main.java

--- Polimorfismo/src/Main.java
+++ Polimorfismo/src/Main.java
@@ -3,12 +3,12 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		Animal animal = new Cachorro();
+		Animal animal = new Cachorro();//cria cachorro dentro de animal
 
-		animal.Falar();
+		animal.Falar();//manda falar
 		
-		animal = new Gato();
-		animal.Falar();
+		animal = new Gato();//cria gato dentro de animal
+		animal.Falar();//manda falar
 		
 		
 	}
